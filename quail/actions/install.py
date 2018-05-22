fd0ef6000ee0955983381345bfbc063303f4d815
Naomi Braun <looseymoose@Naomis-Mistress.local>
2018-05-22 08:48:44 -0400

    Add PHI warning on install

--- quail/actions/install.py
+++ quail/actions/install.py
@@ -12,26 +12,26 @@ def run(root):
          subject_index.db
          quail.conf.yaml
     """
-    root = file_util.full_path(root)
-    print('Installing QUAIL at {}'.format(root))
-    sources_path = file_util.join([root, 'sources'])
-    batch_path = file_util.join([root, 'batches'])
-    subject_index_path = file_util.join([root, 'subject_index.db'])
-    conf_path = file_util.join([root, 'quail.conf.yaml'])
+    verification = input('Using Quail can potentially put you in contact with PHI. Do you understand how to interact with PHI? (y/n)')
+    if verification.lower()=='y' or verification.lower()=='yes':
+        root = file_util.full_path(root)
+        print('Installing QUAIL at {}'.format(root))
+        sources_path = file_util.join([root, 'sources'])
+        batch_path = file_util.join([root, 'batches'])
+        subject_index_path = file_util.join([root, 'subject_index.db'])
+        conf_path = file_util.join([root, 'quail.conf.yaml'])
 
-    conf_data = {
-        'quail_root': root,
-        'sources': {},
-        'batches': {},
-    }
+        conf_data = {
+            'quail_root': root,
+            'sources': {},
+            'batches': {},
+        }
 
-    file_util.mkdir(sources_path)
-    file_util.mkdir(batch_path)
-    file_util.write(subject_index_path)
-    file_util.write(conf_path, conf_data, serialization_format='yaml')
+        file_util.mkdir(sources_path)
+        file_util.mkdir(batch_path)
+        file_util.write(subject_index_path)
+        file_util.write(conf_path, conf_data, serialization_format='yaml')
 
-    print('QUAIL installed at {}'.format(root))
-
-
-
-
+        print('QUAIL installed at {}'.format(root))
+    else:
+        print('Installation cancelled.')
